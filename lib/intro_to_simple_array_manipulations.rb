6907ae47126bbf83b52e95c7644fc6987a79dd78
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-14 21:07:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,15 @@
-array.push
-array.unshift(element)
-array.pop
-array.pop(element)
+def using_push(array,element)
+  array.push(element)
+end
+def using_unshift(array,element)
+  array.unshift(element)
+end 
+def using_pop(array)
+  array.pop
+end
+def pop_with_args(array,element)
+  array.pop(element)
+end
 array.shift 
 array.shift(element)
 array.concat
